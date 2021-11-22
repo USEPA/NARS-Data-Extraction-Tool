3ee111136c67a40f00c0386406d7b18bae159019
Karen Blocksom <blocksom.karen@epa.gov>
2021-11-22 10:07:50 -0800

    Added case where all data in V-3 or V-4 missing/empty.

--- functions/eFormsOrganizeData_byTable_NWCA.r
+++ functions/eFormsOrganizeData_byTable_NWCA.r
@@ -540,6 +540,10 @@ organizeV3.nwca <- function(parsedIn){
                                 'GROUND', 'VEGTYPE'))
     
     aa.out <- subset(aa.long, select = c('SAMPLE_TYPE','PLOT','PARAMETER','RESULT'))
+  }else{
+    
+    aa.out <- data.frame(SAMPLE_TYPE='VEGTYPE', PLOT = NA, 
+                         PARAMETER = NA, RESULT = NA)
   }  
  
  return(aa.out)
@@ -567,7 +571,11 @@ organizeV4.nwca <- function(parsedIn){
     #aa.long$PARAMETER <- with(aa.long, gsub("[:digit:]\\_", "", PARAMETER))
     
     aa.out <- subset(aa.long, select = c('SAMPLE_TYPE','PLOT','V2_LINE','PARAMETER','RESULT'))
-  }  
+  }else{
+    aa.out <- data.frame(SAMPLE_TYPE = 'TREE', PLOT = NA,
+                         V2_LINE = NA, PARAMETER = NA,
+                         RESULT = NA)
+  }   
   
   return(aa.out)
   
